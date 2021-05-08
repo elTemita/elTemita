e2313b1f85848070585a1e390710bbc5aae7822c
Lucas Horestein <lhorestein@gmail,com>
2021-05-08 13:20:42 -0300

    commit Lucas

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,26 +1,23 @@
 ﻿
     function go() {
-       
-
         if (document.form.password.value == 'contra' && document.form.login.value == 'usuario') {
             document.form.submit();
-            alert("OK");
+           // alert("OK");
         }
         else {
             alert("Porfavor ingrese, nombre de usuario y contraseña correctos.");
         }
     }
-
-        var input = document.getElementById("contra");
-    input.addEventListener("keyup", function (event) {
-        //Number 13 is the "Enter" key on the keyboard
-        if (event.keyCode === 13) {
-            // Cancel the default action, if needed
-            event.preventDefault();
-            // Trigger the button element with a click
-            document.getElementById("enviar").click();
-        }
-    });
+    var input = document.getElementById("enviar");
+    input.addEventListener("keyup", function (evento) {
+    //Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("enviar").click();
+    }
+});
 
 function Enviar() {
     var nombre = document.getElementById("nombreBanda");
